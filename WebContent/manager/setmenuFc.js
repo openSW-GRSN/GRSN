95c0ee9df6745a67596b984420644f95ae193815
castlehyun <62753280+castlehyun@users.noreply.github.com>
2021-09-03 23:45:47 +0900

    db에 메뉴판에 넣을 메뉴들을 등록/취소 하는 함수입니다.

---
+++ WebContent/manager/setmenuFc.js
@@ -0,0 +1,49 @@
+var check;
+
+//치즈버거 메뉴판에 추가 또는 빼기
+function addCheeseburger(check){
+	if(check==1){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu1").set("cheeseburger");
+	}
+	else if(check==2){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu1").remove();
+	}
+}
+
+//더블버거 메뉴판에 추가 또는 빼기
+function addDoubleburger(check){
+	if(check==1){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu2").set("doubleburger");
+	}
+	else if(check==2){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu2").remove();
+	}
+}
+
+//케밥버거 메뉴판에 추가 또는 빼기
+function addKebabburger(check){
+	if(check==1){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu3").set("kebabburger");
+	}
+	else if(check==2){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu3").remove();
+	}
+}
+
+//치즈버거 메뉴판에 추가 또는 빼기
+function addCheeseburger(check){
+	if(check==1){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu4").set("cheeseburger");
+	}
+	else if(check==2){
+		var dbRefObject = firebase.database().ref();
+	    dbRefObject.child("setMenu/menu4").remove();
+	}
+}
