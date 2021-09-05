90c2a59507b9f2b1f50d48cc76b5f7eb85181589
castlehyun <62753280+castlehyun@users.noreply.github.com>
2021-09-05 19:38:23 +0900

    자동으로 페이지 넘기기 기능 추가

--- WebContent/js/menu_price2.js
+++ WebContent/js/menu_price2.js
@@ -1,20 +1,20 @@
 // menu_price.js가 임포트 되지 않아 다시 여기서 연결한다 ^0^ (추후에 개선을 해야겠지만...)
 // firebase 연결
 var firebaseConfig = {
-	apiKey: "AIzaSyAhWZp0H5loTHL92JtrXoCEFdwt8s9DDLY",
-	authDomain: "grsn-43bdc.firebaseapp.com",
-	databaseURL: "https://grsn-43bdc-default-rtdb.firebaseio.com",
-	projectId: "grsn-43bdc",
-	storageBucket: "grsn-43bdc.appspot.com",
-	messagingSenderId: "592297355861",
-	appId: "1:592297355861:web:244776f5d914a5c93c02dc",
-	measurementId: "G-60XJK2NTE5"
+    apiKey: "AIzaSyAhWZp0H5loTHL92JtrXoCEFdwt8s9DDLY",
+    authDomain: "grsn-43bdc.firebaseapp.com",
+    databaseURL: "https://grsn-43bdc-default-rtdb.firebaseio.com",
+    projectId: "grsn-43bdc",
+    storageBucket: "grsn-43bdc.appspot.com",
+    messagingSenderId: "592297355861",
+    appId: "1:592297355861:web:244776f5d914a5c93c02dc",
+    measurementId: "G-60XJK2NTE5"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-var path = 'Menu/';		// 경로
+var path = 'Menu/';        // 경로
 var selectMenu = '여기에 메뉴 이름(영문)이 들어가야 함.';
 var menu_name;
 
@@ -23,20 +23,20 @@ var firebase_count_check = firebase.database().ref('Menu');
 firebase_count_check.orderByChild('count').startAt(1).once('child_added', function(data){
     console.log(data.val());
     console.log(data.key);
-	selectMenu = data.key;
-	console.log(selectMenu);
-	
-	// 선택된 메뉴
-	var firebase_menu_name = firebase.database().ref(path + selectMenu + '/name');
-	var menu_name_print = document.getElementById("last_check_menu");
-	
-	firebase_menu_name.on('value', snap => {
-		menu_name = snap.val();
-		menu_name_print.innerHTML =
-			"선택한 메뉴는 " + menu_name + "이며, 추가 요청사항을 다시 한 번 더 확인해주세요.";
-		menu_name_print.style.fontWeight = "900";
-	});
-	
+    selectMenu = data.key;
+    console.log(selectMenu);
+    
+    // 선택된 메뉴
+    var firebase_menu_name = firebase.database().ref(path + selectMenu + '/name');
+    var menu_name_print = document.getElementById("last_check_menu");
+    
+    firebase_menu_name.on('value', snap => {
+        menu_name = snap.val();
+        menu_name_print.innerHTML =
+            "선택한 메뉴는 " + menu_name + "이며, 추가 요청사항을 다시 한 번 더 확인해주세요.";
+        menu_name_print.style.fontWeight = "900";
+    });
+    
 });
 
 // 사이드 메뉴(?) 추가
@@ -47,46 +47,46 @@ var material_add = [];
 var material_list_print;
 
 firebase_material_add.get().then((data)=>{
-	for(var i = 0; i < data.val().length; i++){
-		material_add_bool = data.child(i).val();
-		//console.log(material_add_bool);
-		
-		// material_add 배열에 이름을 부여
-		switch(i){
-			case 0:
-			material_add[0] = "감자튀김(중)";
-			break;
-			
-			case 1:
-			material_add[1] = "음료(중)";
-			break;
-			
-			case 2:
-			material_add[2] = "케첩(+5개)";
-			break;
-			
-			default:
-			break;
-		}
-		// 값에 1이 들어있으면 재료를 추가
-		if(material_add_bool == 1){
-			// material_list_print에 값이 들어있으면,
-			if(material_list_print){
-				material_list_print = material_list_print + ', ' + material_add[i];
-			}else{		// 값이 들어있지 않으면,
-				material_list_print = material_add[i];
-			}
-		}
-	}
-	//console.log(material_list_print);
-	// material_list_print에 아무런 값이 없을 때와 있을 때
-	if(material_list_print == undefined){
-		document.getElementById("total_added_ingredinet").innerHTML =
-		"&nbsp&nbsp추가된 재료가 없습니다.&nbsp;&nbsp;";
-	}else{
-		document.getElementById("total_added_ingredinet").innerHTML =
-			"&nbsp&nbsp추가된 재료:&nbsp" + material_list_print + "&nbsp;&nbsp;";
-	}
+    for(var i = 0; i < data.val().length; i++){
+        material_add_bool = data.child(i).val();
+        //console.log(material_add_bool);
+        
+        // material_add 배열에 이름을 부여
+        switch(i){
+            case 0:
+            material_add[0] = "감자튀김(중)";
+            break;
+            
+            case 1:
+            material_add[1] = "음료(중)";
+            break;
+            
+            case 2:
+            material_add[2] = "케첩(+5개)";
+            break;
+            
+            default:
+            break;
+        }
+        // 값에 1이 들어있으면 재료를 추가
+        if(material_add_bool == 1){
+            // material_list_print에 값이 들어있으면,
+            if(material_list_print){
+                material_list_print = material_list_print + ', ' + material_add[i];
+            }else{        // 값이 들어있지 않으면,
+                material_list_print = material_add[i];
+            }
+        }
+    }
+    //console.log(material_list_print);
+    // material_list_print에 아무런 값이 없을 때와 있을 때
+    if(material_list_print == undefined){
+        document.getElementById("total_added_ingredinet").innerHTML =
+        "&nbsp&nbsp추가된 재료가 없습니다.&nbsp;&nbsp;";
+    }else{
+        document.getElementById("total_added_ingredinet").innerHTML =
+            "&nbsp&nbsp추가된 재료:&nbsp" + material_list_print + "&nbsp;&nbsp;";
+    }
 });
 
 // 가격 읽어오기
@@ -95,6 +95,16 @@ var price_print = document.getElementById("total_price2");
 var price;
 
 firebase_menu_price.on('value', snap =>{
-	price = snap.val();
-	price_print.innerHTML = "&nbsp&nbsp결제할 총 금액은 " + price + "원입니다.&nbsp&nbsp";
+    price = snap.val();
+    price_print.innerHTML = "&nbsp&nbsp결제할 총 금액은 " + price + "원입니다.&nbsp&nbsp";
+});
+
+// 다음 페이지로 넘어갈지 확인
+var firebase_page_check = firebase.database().ref('page_num');
+
+firebase_page_check.on('value', snap =>{
+        var page_num_test = snap.val();
+        if(page_num_test == 1){
+            retnMenuList.click();
+        }
 });
